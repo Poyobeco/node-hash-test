3f3aa135b35c72662edaff9bb78896d01900a534
Luis Felipe <www.barricade@hotmail.com>
2018-09-03 01:59:18 -0500

    Add: Hashtable class
    Hash table definition, pending logic to handle overflows and chaining.

---
+++ sources/hashmap.js
@@ -0,0 +1,11 @@
+
+const Util = require("util"); 
+
+//TODO: Complete definition to allow overflowing map/repeated keys.
+class Hashtable {
+
+}
+
+Util.inherits(Hashtable, Map);
+module.exports = Hashtable;
+
